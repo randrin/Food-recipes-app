09ff1c10e848acde7071deca9efe630d836891c7
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-04-18 15:36:44 +0200

    Set up project

---
+++ src/Recipe.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./recipe.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Recipe() {
+  return (
+    <h1>Recipe App</h1>
+  );
+}
+
+export default Recipe;
